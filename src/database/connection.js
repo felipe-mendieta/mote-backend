6cd3c4dcfe36a10fb40197c225aa947a762406ee
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2023-11-30 01:55:35 -0500

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const config = require('../config/config');
+//root:<password>@engagement.w0cwynd.mongodb.net/
 
-const URI = `mongodb://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 async function getConnection() {
   try {
