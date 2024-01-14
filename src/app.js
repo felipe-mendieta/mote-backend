46754f6404d5f370b935cb819e5c19b6bef3250b
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2024-01-14 09:30:03 -0500

    delete passport libraries

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const optionsCors = require('./config/configCors');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 //passport libraries************************
-const passport = require('passport');
 
 //End passoport librearies******************
 
@@ -16,8 +15,7 @@ const createApp = () => {
   app.use(express.static('public'))
   app.use(express.json());
 
-  app.use(passport.initialize());//iniciamos la sesion
-  app.use(passport.session());//habilitamos el manejo de sesiones
+
   require('./../utils/auth/index');
   //For catch errors and manages status code by https responses
   app.use(logErrors);
