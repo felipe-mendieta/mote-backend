bb37935cad2ea08dee03b7e3dc913edd0c2a2588
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2024-05-22 00:56:52 -0500

    Update connection for node allowed

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 //root:<password>@engagement.w0cwynd.mongodb.net/
 
-const URI = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}`;
+const URI = `mongodb://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 async function getConnection() {
   try {
-    const connection = await mongoose.connect(`${URI}?authSource=admin&readPreference=primary`);
-    return connection;
+    return await mongoose.connect(`${URI}?authSource=admin&readPreference=primary`);
   } catch (error) {
     console.error(URI);
     console.error('Database connection error: ' + error);
