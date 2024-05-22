121378999d199136adf24c13cbbd086ac9f3dce9
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2024-05-22 01:22:25 -0500

    Update configCors.js
    All runs with new lines in whitelists

--- src/config/configCors.js
+++ src/config/configCors.js
@@ -4,13 +4,15 @@ const whitelist = [
   `${config.myDomainApp}:80`,
   `${config.myDomainApp}:4200`,
   `${config.myDomainApp}`,
+  `http://localhost`,
+  `https://mote.ucuenca.edu.ec`,
 ];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
       callback(null, true);//error, permiso : no hay ningun error por eso null, por lo tanto permitir
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error(`Origin: ${origin}\nWhitelist: ${whitelist}`))
     }
   }
 }
